c795d593f724c582d3ec47d820ee409c72d85f45
hariinfo <hariinfo@gmail.com>
2017-12-25 14:10:02 -0600

    updated config

--- claymore/start_etc_lbry.bat
+++ claymore/start_etc_lbry.bat
@@ -22,5 +22,5 @@ IF NOT DEFINED POOL_ETC (SET POOL_ETC=stratum+tcp://us1-etc.ethermine.org:4444)
 IF NOT DEFINED POOL_LBRY (SET POOL_LBRY=stratum+tcp://lbc.coinmine.pl:8787)
 
 
-EthDcrMiner64.exe -epool %POOL_ETC%.%RIG_NAME% -ewal %ADDR_ETC% -epsw x -dcoin lbc -dpool %POOL_LBRY% -dwal %ADDR_LBRY%.%RIG_NAME% -dpsw x -dcri 20
+EthDcrMiner64.exe -epool %POOL_ETC%.%RIG_NAME% -ewal %ADDR_ETC% -allcoins 1 -epsw x -dcoin lbc -dpool %POOL_LBRY% -dwal %ADDR_LBRY%.%RIG_NAME% -dpsw x -dcri 20
 
